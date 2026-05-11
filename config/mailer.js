b98092da642bf57cbf1add8799f022223b923ee3
Krishnasingh9999 <krishna1863singh@gmail.com>
2026-05-11 20:49:34 +0530

    brevo mailer

--- config/mailer.js
+++ config/mailer.js
@@ -1,13 +1,31 @@
+// import nodemailer from "nodemailer";
+
+// const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: process.env.SMTP_USER,
+//     pass: process.env.SMTP_PASS,
+//   },
+//   connectionTimeout: 10000,
+//   greetingTimeout: 10000,
+//   socketTimeout: 10000,
+// });
+
+// export default transporter;
+
+
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
+
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
+
   connectionTimeout: 50000,
   greetingTimeout: 50000,
   socketTimeout: 50000,
