77b1b504c91ed8bce6ce82220bd74d1035f980db
Krishnasingh9999 <krishna1863singh@gmail.com>
2026-05-12 23:20:52 +0530

    sendGrid

--- routes/otpRoutes.js
+++ routes/otpRoutes.js
@@ -55,9 +55,9 @@ router.post("/send-otp", otpLimiter, async (req, res) => {
 
     console.log("📩 Sending OTP email...");
 
-    // DEBUG
-console.log("SMTP USER:", process.env.SMTP_USER);
-console.log("SMTP PASS:", process.env.SMTP_PASS?.slice(0, 10));
+//     // DEBUG
+// console.log("SMTP USER:", process.env.SMTP_USER);
+// console.log("SMTP PASS:", process.env.SMTP_PASS?.slice(0, 10));
 
     // ✅ Send Email (FIXED BLOCK)
     try {
